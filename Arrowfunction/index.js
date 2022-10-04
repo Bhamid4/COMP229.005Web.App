14856f1d8cf746faaf7b4a5665a4102021d1f19c
Bhamid4 <113592135+Bhamid4@users.noreply.github.com>
2022-10-04 17:59:05 -0400

    Arrow1

---
+++ Arrowfunction/index.js
@@ -0,0 +1,13 @@
+var hello;
+ // Arroe function
+hello = () => { 
+    document.getElementById("demo").innerHTML += "Hello 1";
+ } 
+
+  //A button object calls the function: 
+  document.getElementById("btn1").addEventListener("click",hello );
+
+   //A button object calls the function: 
+   document.getElementById("btn2").addEventListener("click", () => { 
+    document.getElementById("demo").innerHTML += "Hello 2";
+ } );
